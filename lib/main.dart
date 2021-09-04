f3d10f0d9c16504a5ab856ddddba589df66e0b42
GhoraiVanga <ghorai77@gmail.com>
2021-09-04 15:04:48 +0530

    succes

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,27 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awsome App"),
       ),
-      body: Container(
-        child: Text("hi flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)]),
+          child: Text(
+            "I am A bOX",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
