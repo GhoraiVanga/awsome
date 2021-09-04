512b6a078f081afc03d76e5965cc8d72243d6194
GhoraiVanga <ghorai77@gmail.com>
2021-09-04 15:27:30 +0530

    succes

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awsome App"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
+      body: Container(
+        height: 500,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
               color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
-              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)]),
-          child: Text(
-            "I am A bOX",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+              alignment: Alignment.center,
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            )
+          ],
         ),
       ),
     );
